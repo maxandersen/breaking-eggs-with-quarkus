2405f389ecf3c531d84fe8f13f5cf363a201c696
Max Rydahl Andersen <max.andersen@gmail.com>
2022-05-04 22:29:36 +0200

    update

---
+++ examples/myservice.java
@@ -0,0 +1,24 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+// Update the Quarkus version to what you want here or run jbang with
+// `-Dquarkus.version=<version>` to override it.
+//DEPS io.quarkus:quarkus-bom:${quarkus.version:1.11.0.Final}@pom
+//DEPS io.quarkus:quarkus-resteasy
+// //DEPS io.quarkus:quarkus-smallrye-openapi
+// //DEPS io.quarkus:quarkus-swagger-ui
+//JAVAC_OPTIONS -parameters
+
+import io.quarkus.runtime.Quarkus;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+@ApplicationScoped
+public class myservice {
+
+    @GET
+    public String sayHello() {
+        return "Hello from Quarkus with jbang.dev";
+    }
+
+}
